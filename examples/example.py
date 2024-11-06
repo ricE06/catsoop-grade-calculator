0a9b5351e94f2150f1a2d870240004c8c3f17b40
ricE06 <ezzhaneri06@gmail.com>
2024-11-05 22:19:48 -0500

    added example file

---
+++ examples/example.py
@@ -0,0 +1,19 @@
+"""
+Sample code to output grade reports.
+"""
+
+from catsoop-grade-calculator import ScoreCalculator
+
+tokens = {}
+with open('token.txt', 'r') as file:
+    for line in file:
+        class_num, token = line.strip().split(' ')
+        tokens[class_num] = token
+
+sooper = ScoreCalculator(tokens)
+report = sooper.report_all()
+print(report)
+
+# to write to a file, uncomment this code:
+# with open("output.txt", "w") as file:
+#     file.write(report)
